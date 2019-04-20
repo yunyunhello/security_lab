5003adac1644c0b69083276074a3aa474a6c89f8
helloyunyun <675803882@qq.com>
2019-04-19 20:24:55 -0400

    server

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ function setup()
 
    // anything below here requires login
    app.use(sessionmanager.isLoggedInMiddleware);
+   app.use(sessionmanager.isAdminUserMiddleware);
 
    // The main page of the app
    app.get("/dashboard", sessionmanager.displayWelcomePage);
@@ -102,8 +103,8 @@ function setup()
    app.post("/contributions", contributions.handleContributionsUpdate);
 
    // Benefits Page
-   app.get("/benefits", sessionmanager.isLoggedInMiddleware, sessionmanager.isAdminUserMiddleware, benefits.displayBenefits);
-   app.post("/benefits", sessionmanager.isLoggedInMiddleware, sessionmanager.isAdminUserMiddleware, benefits.updateBenefits);
+   app.get("/benefits", benefits.displayBenefits);
+   app.post("/benefits", benefits.updateBenefits);
 
    // Allocations Page
    app.get("/allocations/:userId", allocations.displayAllocations);
